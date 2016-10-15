5311bb0c2fe861803d7463454bf6e45431f2f088
Roberto Romo <rromo@berkeley.edu>
2016-10-15 12:31:30 -0700

    step 2

---
+++ step2.py
@@ -0,0 +1,24 @@
+import requests;
+
+api_token = "98167604da0984648f3b69b863562265"
+endpoint = "http://challenge.code2040.org/api/reverse"
+post_to = "http://challenge.code2040.org/api/reverse/validate"
+
+## Essentially 'string'[::-1]
+## Reverse a string
+def reverse(string):
+	start = end = None
+	step = -1
+	slice_rev = slice(start, end, step)
+	return string[slice_rev]
+
+
+
+ r_1 = requests.post(endpoint, data = {'token': api_token})
+ reverse_api= reverse(r_1.text)
+
+ r_2 = requests.post(post_to, data = {'token': api_token, 'reversed': reverse_api})
+ print(r_2.text)
+
+
+
